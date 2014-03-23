042664d953c7609f7269018a969efeb0f39cd56d
Jonathan Czorny <jonathan.czorny@gmail.com>
2014-03-23 21:03:40 +0100

    maj mongo + tests

--- start.js
+++ start.js
@@ -1,15 +1,30 @@
 var http = require("http");
+var mysql = require('mysql');
 var controller = require("./controllers/controller");
 
 var securiteService = require("./services/securiteService");
 var errorService = require("./services/errorService");
 var requestService = require("./services/requestService");
 var templateService = require("./services/templateService");
-//var benchmark = require("./services/benchmark");
+var benchmark = require("./services/benchmark");
 
+var mongo = require('mongodb');
 
+var MongoClient = mongo.MongoClient;
+var Db = mongo.Db,Connection = mongo.Connection,Server = mongo.Server;
+var myCollection;
+var db = MongoClient.connect("mongodb://localhost:"+Connection.DEFAULT_PORT+"/vmedias", function(err, db) {
+	if(err)
+		throw err;
+	console.log("connecter avec MongoDB !");
+	myCollection = db.collection('user');
+	myCollection.find().toArray(function(err, results){
+		if(err)
+			throw err;
+		console.log("results :", results);
+	});
+});
 
 controller.Init(securiteService, securiteService, requestService, templateService);
 
 http.createServer(function (request, response) { controller.ExecuteRequest(request, response) }).listen(8080);
-
