372b5c061b0cf05efb9dfa04f73bef833b3849bc
Dacer08 <dacer.fze>
2014-03-23 19:40:17 +0100

    Save

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,11 +1,23 @@
+var mysql = require('mysql');
+
 function homeController() { }
 
 homeController.prototype = {
 	Init: function () {
 	},
 	index: function () {
+		var client = mysql.createConnection({
+			host: 'f70969e1-ba35-42aa-a31a-a2e801344c87.mysql.sequelizer.com',
+			user: 'oozlsaowbpyfpjje',
+			password: 'UPFnynJ7PtNnBwDmSegtBgYAx8YJmQGBXkjentAu85UwvuyNgTCFpcQNXeWyv8T7',
+			database: 'dbf70969e1ba3542aaa31aa2e801344c87',
+		});
 
-		return process.env.OPENSHIFT_MYSQL_DB_URL || "looooooooooooooooooool";
+		client.connect(function (err) {
+			var query = client.query('select * from valentinosony', function (err, result) {
+				return result;
+			});
+		});
 	},
 	detail: function () {
 		return "";
